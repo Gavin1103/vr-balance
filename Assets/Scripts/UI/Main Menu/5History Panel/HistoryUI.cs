28e2d9de9d8391b1286ef335362c2424dbe71cd1
Amine <amine.el.hammdaoui@hva.nl>
2025-06-24 22:48:16 +0200

    History UI wordt nu dynamishc veranderd als het leeg is

--- Assets/Scripts/UI/Main Menu/5History Panel/HistoryUI.cs
+++ Assets/Scripts/UI/Main Menu/5History Panel/HistoryUI.cs
@@ -1,29 +1,40 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.UI;
 public class HistoryUI : MonoBehaviour {
     public Transform historyContainer;
     public GameObject historyEntryPrefab;
+    public GameObject noHistoryFoundText;
 
     void OnEnable() {
         StartCoroutine(FetchAndDisplayHistory());
     }
 
     private IEnumerator FetchAndDisplayHistory() {
-        // Ensure the history container is empty before populating it
-        for (int i = historyContainer.childCount - 1; i >= 0; i--)
+        // Ensure the history container is empty (except ze first one) before populating it
+        for (int i = historyContainer.childCount - 1; i >= 1; i--)
             Destroy(historyContainer.GetChild(i).gameObject);
 
+        noHistoryFoundText.SetActive(true);
+        noHistoryFoundText.GetComponent<TMP_Text>().text = "Exercises don't get saved when playing as a guest.";
 
         ExerciseService exerciseService = new ExerciseService();
-        // Destroy it first
         yield return exerciseService.FetchLast10Exercises(
             onSuccess: response =>
             {
                 // Clear previous entries (except header if you have one)
                 for (int i = historyContainer.childCount - 1; i >= 1; i--)
                     Destroy(historyContainer.GetChild(i).gameObject);
+
+                    if (response.data == null || response.data.Count == 0) {
+                        noHistoryFoundText.SetActive(true);
+                        noHistoryFoundText.GetComponent<TMP_Text>().text = "No exercise history found because you have never done anything. Play your first exercise!";
+                        return;
+                    } else {
+                        noHistoryFoundText.SetActive(false);
+                    }
+
 
                 foreach (var exercise in response.data)
                 {
@@ -37,6 +48,7 @@ public class HistoryUI : MonoBehaviour {
             onError: err =>
             {
                 Debug.Log(err.message);
+                noHistoryFoundText.GetComponent<TMP_Text>().text = "Something went wrong trying to fetch an exercise. Error message: " + err.message;
                 Debug.LogError("Failed to fetch exercise history: " + err);
             }
         );
