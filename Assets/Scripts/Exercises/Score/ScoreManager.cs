f253c44140304d3f46da90d2fad97c3a767c8a13
Amine <amine.el.hammdaoui@hva.nl>
2025-06-28 21:54:44 +0200

    Removed debug

--- Assets/Scripts/Exercises/Score/ScoreManager.cs
+++ Assets/Scripts/Exercises/Score/ScoreManager.cs
@@ -9,10 +9,8 @@ public class ScoreManager : MonoBehaviour {
         Instance = this;
     }
 
-    public void AddScore(float amount)
-    {
+    public void AddScore(float amount) {
         Score += amount;
-        Debug.Log(Score);
     }
 
     public void ResetScore() {
