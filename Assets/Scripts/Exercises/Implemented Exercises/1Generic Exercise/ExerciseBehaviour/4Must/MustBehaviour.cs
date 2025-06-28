3062cbf2df9f767c016d9dc40e9f9dfd4a4c5087
Amine <amine.el.hammdaoui@hva.nl>
2025-06-28 12:51:32 +0200

    De must movement is gereworked. Je moet nu een bepaalde hoeveelheid seconden (standaard 3) de movement vasthouden terwijl het accurate enough is (meer dan 80% accuraat). Dit laat de glute bridge veel beter voelen

--- Assets/Scripts/Exercises/Implemented Exercises/1Generic Exercise/ExerciseBehaviour/4Must/MustBehaviour.cs
+++ Assets/Scripts/Exercises/Implemented Exercises/1Generic Exercise/ExerciseBehaviour/4Must/MustBehaviour.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using UnityEngine;
 
 public class MustBehaviour : IMovementBehaviour {
-    public MustBehaviour() { 
-        
+    private float holdTime;
+    private float elapsedWhileHolding = 0f;
+    public MustBehaviour() {
+        GenericExerciseReferences.Instance.InformationText.text = holdTime.ToString("0.0") + "s";
+        GenericExerciseReferences.Instance.InformationObject.SetActive(true);
     }
     
     public override void OnMovementStart(ExerciseMovement movement) {
@@ -13,20 +16,19 @@ public class MustBehaviour : IMovementBehaviour {
     
     public override IEnumerator OnMovementUpdate(ExerciseMovement movement) {
         while (true) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                break;
-            }
+            if (Input.GetKeyDown(KeyCode.Space)) break;
 
             movement.exercise.ScoreCalculator.CalculateDistances(movement.LeftStickTarget, movement.RightStickTarget, movement.HeadTarget);
-            if (movement.exercise.ScoreCalculator.AllTargetsHit()) {
-                if (movement.exercise.ScoreCalculator.IsAccurate()) {
-                    break; // Continue only when target positions are accurately hit
-                } else {
-                    GenericExerciseReferences.Instance.InformationText.text = "Not Accurate Enough!";
-                }
+
+            if (movement.exercise.ScoreCalculator.AllTargetsHit() && movement.exercise.ScoreCalculator.IsAccurate()) {
+                elapsedWhileHolding += Time.deltaTime;
+                GenericExerciseReferences.Instance.InformationText.text = (holdTime - elapsedWhileHolding).ToString("0.0") + "s";
             } else {
-                GenericExerciseReferences.Instance.InformationText.text = "Must Complete";
+                elapsedWhileHolding = 0f;
+                GenericExerciseReferences.Instance.InformationText.text = "Not Accurate Enough!";
             }
+
+            if (elapsedWhileHolding >= holdTime) break;
 
             yield return null;
         }
