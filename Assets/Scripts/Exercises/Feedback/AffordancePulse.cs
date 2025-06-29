c5e7836e49e27e79c27b284c10b1bf607cbd1cb8
Amine <amine.el.hammdaoui@hva.nl>
2025-06-29 15:13:57 +0200

    pulse effect is nu juicy goed

--- Assets/Scripts/Exercises/Feedback/AffordancePulse.cs
+++ Assets/Scripts/Exercises/Feedback/AffordancePulse.cs
@@ -16,6 +16,16 @@ public class AffordancePulse : MonoBehaviour
         originalScale = transform.localScale;
         mat = GetComponent<Renderer>().material;
         originalOpacity = mat.color.a;
+
+        mat.SetFloat("_Mode", 3); // 3 = Transparent in Standard shader
+        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        mat.SetInt("_ZWrite", 0);
+        mat.DisableKeyword("_ALPHATEST_ON");
+        mat.EnableKeyword("_ALPHABLEND_ON");
+        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        mat.renderQueue = 3000;
+
     }
 
     void OnEnable()
